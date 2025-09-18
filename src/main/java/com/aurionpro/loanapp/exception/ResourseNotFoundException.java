6cf3db7259013c79fc704ebf3aa6bdba1896c5d3
Karan-Mharse <krnmharse06@gmail.com>
2025-09-18 17:18:27 +0530

    Basic project folder

---
+++ src/main/java/com/aurionpro/loanapp/exception/ResourseNotFoundException.java
@@ -0,0 +1,8 @@
+package com.aurionpro.loanapp.exception;
+
+public class ResourseNotFoundException extends RuntimeException {
+	public ResourseNotFoundException(String message) {
+		super(message);
+	}
+
+}
