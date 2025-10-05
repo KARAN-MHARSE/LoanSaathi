2696c0d771d950d2f9265193f123f8d91c4437d1
Karan-Mharse <krnmharse06@gmail.com>
2025-10-05 20:22:30 +0530

    Get loan by application id added

--- Backend/src/main/java/com/aurionpro/loanapp/controller/LoanApplicationController.java
+++ Backend/src/main/java/com/aurionpro/loanapp/controller/LoanApplicationController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -85,6 +87,14 @@ public class LoanApplicationController {
 		return ResponseEntity.ok(response);
 		
 	}
+	
+	@GetMapping("/{applicationId}")
+    @PreAuthorize("hasAnyRole('ROLE_CUSTOMER', 'ROLE_OFFICER')")
+    public ResponseEntity<Object> getLoanApplicationById(@PathVariable Long applicationId, Authentication authentication) {
+		LoanApplicationResponseDto applicationDTO = loanApplicationService.getApplicationById(applicationId, authentication);
+        return ResponseEntity.ok(applicationDTO);
+    }
+	
 	
 	
 
