2982590c3eebf6238aad8c9064057be641ee407c
Prathamesh Patil <prathamesh07723@gmail.com>
2025-10-14 22:07:10 +0530

    Added Officer

--- Frontend/src/app/modules/admin/admin-routing-module.ts
+++ Frontend/src/app/modules/admin/admin-routing-module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
-import { AddLoanScheme } from './modules/loan-scheme/pages/add-loan-scheme/add-loan-scheme';
-import { AllLoanSchemes } from './modules/loan-scheme/pages/all-loan-schemes/all-loan-schemes';
-import { LoanSchemeDetail } from './modules/loan-scheme/pages/loan-scheme-detail/loan-scheme-detail';
-import { UpdateLoanScheme } from './modules/loan-scheme/pages/update-loan-scheme/update-loan-scheme';
-import { AllOfficers } from './modules/officer/pages/all-officers/all-officers';
-import { OfficerDetails } from './modules/officer/pages/officer-details/officer-details';
-import { AddNewOfficer } from './modules/officer/pages/add-new-officer/add-new-officer';
 import { Profile } from './pages/profile/profile';
-import { UpdateOfficerDetails } from './modules/officer/pages/update-officer-details/update-officer-details';
-import { OfficerHandleLoans } from './modules/officer/pages/officer-handle-loans/officer-handle-loans';
 
 const routes: Routes = [
   {
@@ -32,7 +23,7 @@ const routes: Routes = [
       return import("./modules/loan-scheme/loan-scheme-module").then(m=>m.LoanSchemeModule);
     }
   },
-   {
+  {
     path:"officers",
     loadChildren:()=>{
       return import("./modules/officer/officer-module").then(m=>m.OfficerModule);
--- Frontend/src/app/modules/officer/officer-routing-module.ts
+++ Frontend/src/app/modules/officer/officer-routing-module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Profile } from './pages/profile/profile';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'profile',
+    component: Profile,
+  },
+  {
+    path: 'application',
+    loadChildren: () => {
+      return import('./modules/loan-application/loan-application-module').then(
+        (m) => m.LoanApplicationModule
+      );
+    },
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class OfficerRoutingModule { }
+export class OfficerRoutingModule {}
